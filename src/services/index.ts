f34f3cbf0a861120b069cc13293277d9c6a5aa29
BipRaider <42334253+BipRaider@users.noreply.github.com>
2021-05-26 18:04:31 +0300

    2 commit

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://pacific-sands-67465.herokuapp.com';
+axios.defaults.baseURL = 'https://rstestbe.herokuapp.com/';
 axios.defaults.headers.get['Accept'] = 'application/json';
 
 const fetchApi = {
