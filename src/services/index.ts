920bf883ae04762d9e368c9fc3959dfe1d1fbacb
BipRaider <42334253+BipRaider@users.noreply.github.com>
2021-06-05 14:18:26 +0300

    added url backend

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import env from 'react-dotenv';
 
-axios.defaults.baseURL = env.API_URL;
+axios.defaults.baseURL = env.API_URL||'https://rstestbe.herokuapp.com';
 axios.defaults.headers.get['Accept'] = 'application/json';
 
 const fetchApi = {
